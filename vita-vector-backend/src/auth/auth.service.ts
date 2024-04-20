707a808325fce364d2c1157a743130fdd4b44559
Romkoos <roman.neganov@gmail.com>
2024-04-20 12:41:35 +0300

    auth infra

--- vita-vector-backend/src/auth/auth.service.ts
+++ vita-vector-backend/src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import { AuthDto } from '../dto/auth.dto';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+  async login(dto: AuthDto) {
+    return dto;
+  }
+  //   const user = await this.userService.getByEmail(dto.email);
+  //   if (!user) {
+  //     return null;
+  //   }
+  //   return {
+  //     token: this.jwtService.sign({ id: user.id }),
+  //     user,
+  //   };
+  // }
+}
